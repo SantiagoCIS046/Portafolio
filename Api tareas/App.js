4577eb98f18db7d8d8016a1b301704a2bcc07447
Santiago Cisneros <santiagocisneros046@gmail.com>
2025-10-27 18:18:33 -0500

    Se agregan las carpetas

--- Api tareas/App.js
+++ Api tareas/App.js
@@ -1,13 +1,26 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { setupRoutes } from "./Rutas/rutas.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 
+// Serve static files from root directory
+app.use(express.static(__dirname));
+
+// Serve index.html at root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 setupRoutes(app);
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
